1d9d9cd662ea43e73cc5a55633ba4bb63bf30d1b
olektomek <olekstomek@gmail.com>
2019-05-16 23:35:07 +0200

    mała poprawka - domyślny modyfikator klasy jak w oryginale; formatowanie

--- src/main/java/pl/javastart/Runner.java
+++ src/main/java/pl/javastart/Runner.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class Runner {
+class Runner {
 
     public static void main(String[] args) {
 
